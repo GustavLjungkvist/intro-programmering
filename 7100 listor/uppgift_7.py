00061f62dabb30af392c20a830c868cef02abd9c
Gustav Ljungkvist <gustav.ljungqvist06@skola.stenungsund.se>
2025-02-07 15:06:24 +0100

    s

--- 7100 listor/uppgift_7.py
+++ 7100 listor/uppgift_7.py
@@ -9,28 +9,31 @@ def hand_value(hand):
     return value - 10 if value > 21 and 11 in hand else value
 
 def blackjack():
-    player_hand = [draw_card(), draw_card()]
-    dealer_hand = [draw_card()]
+    while True:
+        player_hand = [draw_card(), draw_card()]
+        dealer_hand = [draw_card()]
 
-    print(f"Din hand: {player_hand}, poäng: {hand_value(player_hand)}")
-    while hand_value(player_hand) < 21 and input("Hit (h) eller Stand (s)? ") == 'h':
-        player_hand.append(draw_card())
         print(f"Din hand: {player_hand}, poäng: {hand_value(player_hand)}")
-        
-    if hand_value(player_hand) > 21:
-        print("Du blev tjock! Dealern vinner.")
-        return
+        while hand_value(player_hand) < 21 and input("Hit (h) eller Stand (s)? ") == 'h':
+            player_hand.append(draw_card())
+            print(f"Din hand: {player_hand}, poäng: {hand_value(player_hand)}")
 
-    while hand_value(dealer_hand) < 17:
-        dealer_hand.append(draw_card())
+        if hand_value(player_hand) > 21:
+            print("Du blev tjock! Dealern vinner.")
+        else:
+            while hand_value(dealer_hand) < 17:
+                dealer_hand.append(draw_card())
 
-    print(f"Dealerns hand: {dealer_hand}, poäng: {hand_value(dealer_hand)}")
+            print(f"Dealerns hand: {dealer_hand}, poäng: {hand_value(dealer_hand)}")
 
-    if hand_value(dealer_hand) > 21 or hand_value(player_hand) > hand_value(dealer_hand):
-        print("Du vinner!")
-    elif hand_value(player_hand) < hand_value(dealer_hand):
-        print("Dealern vinner!")
-    else:
-        print("Oavgjort!")
+            if hand_value(dealer_hand) > 21 or hand_value(player_hand) > hand_value(dealer_hand):
+                print("Du vinner!")
+            elif hand_value(player_hand) < hand_value(dealer_hand):
+                print("Dealern vinner!")
+            else:
+                print("Oavgjort!")
+
+        if input("Vill du spela igen? j/n: ") != 'j':
+            break
 
 blackjack()
